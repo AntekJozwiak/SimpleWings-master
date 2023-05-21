e7a590769b109de21b86f8554afce4cbffbdea5e
thehumbleonion <93240507+thehumbleonion@users.noreply.github.com>
2023-05-21 18:37:08 +0700

    Added Braking

--- Assets/SimpleWings/Demo/Scripts/GearVisuals.cs
+++ Assets/SimpleWings/Demo/Scripts/GearVisuals.cs
@@ -8,43 +8,53 @@ using System.Collections.Generic;
 
 public class GearVisuals : MonoBehaviour
 {
-	public WheelCollider[] wheels;
-	public Transform wheelVisualizerPrefab;
+    public WheelCollider[] wheels;
+    public Transform wheelVisualizerPrefab;
 
-	private Dictionary<Transform, WheelCollider> visualToWheelMap;
+    private Dictionary<Transform, WheelCollider> visualToWheelMap;
 
-	private void Awake()
-	{
-		visualToWheelMap = new Dictionary<Transform, WheelCollider>();
-	}
+    private void Awake()
+    {
+        visualToWheelMap = new Dictionary<Transform, WheelCollider>();
+    }
 
-	// Use this for initialization
-	private void Start()
-	{
-		if (wheelVisualizerPrefab != null)
-		{
-			// Create a cylinder and associate each cylinder with a wheel.
-			foreach (WheelCollider wheel in wheels)
-			{
-				Transform visual = Instantiate(wheelVisualizerPrefab, wheel.transform);
-				visualToWheelMap.Add(visual, wheel);
-			}
-		}
-	}
+    private void Start()
+    {
+        if (wheelVisualizerPrefab != null)
+        {
+            // Create a cylinder and associate each cylinder with a wheel.
+            foreach (WheelCollider wheel in wheels)
+            {
+                Transform visual = Instantiate(wheelVisualizerPrefab, wheel.transform);
+                visualToWheelMap.Add(visual, wheel);
+            }
+        }
+    }
 
-	private void Update()
-	{
-		if (visualToWheelMap.Count > 0)
-		{
-			Vector3 pos;
-			Quaternion rot;
+    private void Update()
+    {
+        if (visualToWheelMap.Count > 0)
+        {
+            Vector3 pos;
+            Quaternion rot;
 
-			foreach (var visualWheel in visualToWheelMap)
-			{
-				visualWheel.Value.GetWorldPose(out pos, out rot);
-				visualWheel.Key.position = pos;
-				visualWheel.Key.rotation = rot;
-			}
-		}
-	}
+            foreach (var visualWheel in visualToWheelMap)
+            {
+                visualWheel.Value.GetWorldPose(out pos, out rot);
+                visualWheel.Key.position = pos;
+                visualWheel.Key.rotation = rot;
+
+                // Apply brakes when Fire2 axis is pressed
+                if (Input.GetButton("Fire2"))
+                {
+                    visualWheel.Value.brakeTorque = 100f; // Adjust the brake torque as needed
+                }
+                else
+                {
+                    visualWheel.Value.brakeTorque = 0f;
+                }
+            }
+        }
+    }
 }
+
